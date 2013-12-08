c46c42459aec1b8e3d81adf4b02561ec0c5dcd5e
Thiago Lioy <thiago.lioy@concretesolutions.com.br>
2013-12-08 13:41:14 -0200

    Created xcode project

---
+++ Events/Events/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Events
+//
+//  Created by Thiago Lioy on 12/8/13.
+//  Copyright (c) 2013 Thiago Lioy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
