a555a4c5f709e3d165424ea2758136a93630f74b
Thiago Lioy <lioyufrj@gmail.com>
2013-12-08 14:26:43 -0200

    Added constants and keys

---
+++ Events/Events/Constants.h
@@ -0,0 +1,12 @@
+//
+//  Constants.h
+//  Events
+//
+//  Created by Thiago Lioy on 12/8/13.
+//  Copyright (c) 2013 Thiago Lioy. All rights reserved.
+//
+
+#ifndef Events_Constants_h
+#define Events_Constants_h
+
+#endif
---
+++ Events/Events/Keys.h
@@ -0,0 +1,14 @@
+//
+//  Keys.h
+//  Events
+//
+//  Created by Thiago Lioy on 12/8/13.
+//  Copyright (c) 2013 Thiago Lioy. All rights reserved.
+//
+
+#ifndef Events_Keys_h
+#define Events_Keys_h
+
+#define APP_KEY @"{Your App Key}"
+
+#endif
